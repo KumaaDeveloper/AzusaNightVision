502168ce7dff2f77c0cb950ed047ccb7bb15302b
FunkaRevon <ardrafunka@gmail.com>
2024-06-26 23:31:32 +0700

    Add files via upload

--- src/KumaDev/NightVision/Main.php
+++ src/KumaDev/NightVision/Main.php
@@ -8,48 +8,73 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\player\Player;
 use pocketmine\entity\effect\VanillaEffects;
-use pocketmine\utils\Config;
+use pocketmine\entity\effect\EffectInstance;
 
 class Main extends PluginBase implements Listener {
 
-    private bool $nightVisionEnabled;
+    private string $mode;
+    private array $worlds;
     private int $checkInterval;
 
     public function onEnable(): void {
         $this->saveDefaultConfig();
-        $this->nightVisionEnabled = $this->getConfig()->get("NightVision", true);
-        $this->checkInterval = $this->getConfig()->get("CheckInterval", 60);
+        $this->mode = $this->getConfig()->get("Mode", "whitelist");
+        $this->worlds = $this->getConfig()->get("Worlds", []);
+        $this->checkInterval = $this->getConfig()->get("CheckInterval", 120);
 
-        if ($this->nightVisionEnabled) {
-            $this->getServer()->getPluginManager()->registerEvents($this, $this);
-            $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function (): void {
-                foreach($this->getServer()->getOnlinePlayers() as $player) {
-                    $this->giveNightVision($player);
-                }
-            }), 20 * $this->checkInterval); // Repeat based on config interval
-        }
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function (): void {
+            foreach ($this->getServer()->getOnlinePlayers() as $player) {
+                $this->updateNightVisionEffect($player);
+            }
+        }), 20 * $this->checkInterval); // Repeat based on config interval
     }
 
     public function onJoin(PlayerJoinEvent $event): void {
-        if ($this->nightVisionEnabled) {
-            $player = $event->getPlayer();
-            $this->giveNightVision($player);
-        }
+        $player = $event->getPlayer();
+        $this->updateNightVisionEffect($player);
     }
 
     public function onRespawn(PlayerRespawnEvent $event): void {
-        if ($this->nightVisionEnabled) {
-            $player = $event->getPlayer();
+        $player = $event->getPlayer();
+        $this->updateNightVisionEffect($player);
+    }
+
+    public function onMove(PlayerMoveEvent $event): void {
+        $player = $event->getPlayer();
+        $this->updateNightVisionEffect($player);
+    }
+
+    private function shouldGiveNightVision(Player $player): bool {
+        $worldName = $player->getWorld()->getFolderName();
+        if ($this->mode === "whitelist") {
+            return in_array($worldName, $this->worlds, true);
+        } elseif ($this->mode === "blacklist") {
+            return !in_array($worldName, $this->worlds, true);
+        }
+        return false;
+    }
+
+    private function updateNightVisionEffect(Player $player): void {
+        if ($this->shouldGiveNightVision($player)) {
             $this->giveNightVision($player);
+        } else {
+            $this->clearNightVision($player);
         }
     }
 
     private function giveNightVision(Player $player): void {
         $nightVisionEffect = VanillaEffects::NIGHT_VISION();
-        $effectInstance = new \pocketmine\entity\effect\EffectInstance($nightVisionEffect, 2147483647, 0, false, false); // Particles set to false
+        $effectInstance = new EffectInstance($nightVisionEffect, 2147483647, 0, false, false); // Particles set to false
         $player->getEffects()->add($effectInstance);
     }
+
+    private function clearNightVision(Player $player): void {
+        $player->getEffects()->remove(VanillaEffects::NIGHT_VISION());
+    }
 }
+
